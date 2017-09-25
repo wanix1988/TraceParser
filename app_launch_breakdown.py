13a7484f69f552cbf0069409b3bf32c50018187b
linwei.wang <wanix1988@gmail.com>
2017-09-25 09:25:11 +0800

    添加清除临时文件代码，修复生成统计结果有误的问题（部分），修改部分log开关

--- app_launch_breakdown.py
+++ app_launch_breakdown.py
@@ -78,7 +78,7 @@ def __init_sys_trace():
 														launcher_pkg_name=OPPO_HELLOWORLD_PKG_NAME))
 			continue
 		IMPORTANT_SYS_KEY_FLOW_POINT.append(line.format(prefix=PROCESS_SYS_TRACE_COMMON_PREFIX))
-	print 'sys_key:', IMPORTANT_SYS_KEY_FLOW_POINT
+	#print 'sys_key:', IMPORTANT_SYS_KEY_FLOW_POINT
 	print 'Sys Trace init DONE!!!'
 
 def __init_app_trace(main_thread_pid):
@@ -115,7 +115,7 @@ def find_helloworld_main_thread(trace):
 	helloworld_main_thread_pattern = r'-(\d+)\s+\[.*?tracing_mark_write:\s+B\|\1\|.*?testlaunch'
 	return find_main_thread(trace, helloworld_main_thread_pattern)
 
-def filter_main_thread_trace(trace, main_thread_pid, output=r'main_thread.trace'):
+def filter_main_thread_trace(trace, main_thread_pid, output=r'.main_thread.trace'):
 	'''
 	过滤trace中的主线程信息， 存入文件备查
 	@trace systrace文件
@@ -175,9 +175,9 @@ def calc_main_thread_key_flow_gap(main_thread_trace_output):
 	'''
 	global result_file
 	stack = []
-	with open(main_thread_trace_output) as rf, open(op.join(op.dirname(main_thread_trace_output), result_file), 'wb+') as wf:
-		print 'app trace begin pattern:', APP_TRACE_BEGIN_PATTERN
-		print 'app trace end pattern:', APP_TRACE_END_PATTERN
+	with open(main_thread_trace_output) as rf, open(op.join(op.dirname(main_thread_trace_output), result_file), 'ab+') as wf:
+		#print 'app trace begin pattern:', APP_TRACE_BEGIN_PATTERN
+		#print 'app trace end pattern:', APP_TRACE_END_PATTERN
 		for line in rf:
 			#print 'line:', line
 			matcher = re.search(APP_TRACE_BEGIN_PATTERN, line, re.DOTALL)
@@ -185,7 +185,7 @@ def calc_main_thread_key_flow_gap(main_thread_trace_output):
 				#print 'BEGIN', matcher.group()
 				# 所有B节点都需入栈, 模式为第一段为文本行内容，第二段为时间戳，第三段是trace内容
 				begin_content = line+'@#'+matcher.group(2)+'#@'+matcher.group(3)
-				#print 'begin_content:', begin_content
+				print 'begin_content:', begin_content
 				stack.append(begin_content)
 			else:
 				matcher = re.search(APP_TRACE_END_PATTERN, line, re.DOTALL)
@@ -200,7 +200,7 @@ def calc_main_thread_key_flow_gap(main_thread_trace_output):
 						top = stack.pop()
 						top_content = top.split('@#')
 						top_line = top_content[0]
-						#print 'top_line:', top_line
+						print 'top_line:', top_line
 						top_content_2 = top_content[1].split('#@')
 						top_timestamp = top_content_2[0]
 						top_keyword = top_content_2[1]
@@ -240,7 +240,7 @@ def get_sys_key_flow_data(trace_file):
 	global result_file
 	result_file_full_path = op.join(op.dirname(trace_file), result_file)
 	#print 'sys key flow result file:', result_file_full_path
-	with open(trace_file) as rf, open(result_file_full_path, 'wb+') as wf:
+	with open(trace_file) as rf, open(result_file_full_path, 'ab+') as wf:
 		wf.write(trace_file+'\n')
 		for line in rf:
 			if line.startswith('#'):
@@ -251,8 +251,9 @@ def get_sys_key_flow_data(trace_file):
 				if matcher:
 					result = matcher.group(3)
 					timestamp = matcher.group(1)
-					wf.write(','.join([result, timestamp, '']))
-					wf.write('\n')
+					content = ','.join([result, timestamp, ''])
+					print 'content:', content
+					wf.write(content+'\n')
 					break
 	print 'sys key flow parse DONE!!!'
 
@@ -275,7 +276,7 @@ def drop_systrace_html(trace, output=r'.raw.trace'):
 	'''
 	begin_trace = False
 	output_file = op.join(op.dirname(trace), op.basename(trace).split('.')[0] + output)
-	with open(trace) as rf, open(output_file, 'wb') as wf:
+	with open(trace) as rf, open(output_file, 'wb+') as wf:
 		for line in rf:
 			if BEGIN_TRACE_TAG in line:
 				begin_trace = True
@@ -283,21 +284,26 @@ def drop_systrace_html(trace, output=r'.raw.trace'):
 				wf.write(line)
 			if END_TRACE_TAG in line:
 				break
+	print begin_trace, output_file
 	return begin_trace, output_file
 	
+def clean_temp_files(folder):
+	'''
+	清除指定目录下的临时文件
+	'''
+	clean_files = ['.raw.trace', '.main_thread.trace', '.csv']
+	for root, dirs, files in os.walk(folder):
+		for f in files:
+			for p in clean_files:
+				if p in f:
+					os.remove(op.join(root, f))
+	print 'Clean DONE!!!'
+	
 if __name__ == '__main__':
-	#if len(sys.argv) != 3:
-	#	print 'Usage: %s trace' % sys.argv[0]
-	#	sys.exit(-1)
-	#__init_trace(sys.argv[2])
-	#app_launch_breakdown(sys.argv[1], sys.argv[2])
-	#for local test X230T
-	#__init_trace('20302')
-	#app_launch_breakdown('/home/wenix/work/mtk/performance/oppo-helloworld/oppo-helloworld.html', '20302')
-	#for local L440
 	IGNORE_FILES = ['.rar', '.csv', '.trace']
 	folder = sys.argv[1]
 	__init_sys_trace()
+	clean_temp_files(sys.argv[1])
 	if op.isdir(folder):
 		for root, dirs, files in os.walk(folder):
 			for f in files:
